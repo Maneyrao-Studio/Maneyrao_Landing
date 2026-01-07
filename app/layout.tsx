7dca73f33aa39c1ad47a1cc61123e2299a7bd335
Ciro Mirkin <ciromirkin@gmail.com>
2026-01-07 19:31:42 -0300

    agrega: funcion generateMetadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,20 @@ import { cn } from "@/lib/utils"
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
 
-export const metadata: Metadata = {
-  title: "Maneyrao Studio",
-  description: "Shaping the future of digital experiences.",
-    generator: 'v0.app'
+const ourWebsiteUrl = "https://www.maneyrao.vercel.app/"
+
+export async function generateMetadata(): Promise<Metadata> {
+  const title = "Maneyrao Studio"
+
+  const description =
+    "Nos enfocamos en brindar claridad, ayudar a exponer e integrar sistemas que se adapten y optimicen tu forma de trabajar."
+
+  return {
+    metadataBase: new URL(ourWebsiteUrl),
+    title,
+    description,
+    themeColor: "black"
+  }
 }
 
 export default function RootLayout({
@@ -17,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="es" className="dark">
       <body className={cn("min-h-screen bg-black font-sans antialiased selection:bg-white/20", inter.variable)}>
         {children}
       </body>
